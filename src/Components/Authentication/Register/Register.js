f960fdd10c6a4cdb66d89b55c56cfdac5c2ad549
LubomirP <lubomirpacheliev@gmail.com>
2022-07-20 16:23:28 +0300

    basic register form

---
+++ src/Components/Authentication/Register/Register.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Register.scss';
+
+// TODO: Notification Component
+// TODO: Field requirements for password
+const Register = () => {
+    return (
+        <section className="register-page">
+            <form action="/register" method="POST">
+                <label htmlFor="username">Username</label>
+                <input type="text" name='username' id='username' placeholder='eveline57' />
+
+                <label htmlFor="email">Email</label>
+                <input type="text" name='email' id='email' placeholder='eveline@gmail.com' />
+
+                <label htmlFor="password">Password</label>
+                <input type="text" name='password' id='password' placeholder='******' />
+
+                <label htmlFor="re-password">Confirm Password</label>
+                <input type="text" name='re-password' id='re-password' placeholder='******' />
+
+                <input type="submit" value="Register" />
+                <div class="card-already-have-account">
+                    <a href='/login'>Already have an account?</a>
+                </div>
+            </form>
+        </section>
+    );
+}
+
+export default Register;
